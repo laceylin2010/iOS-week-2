9a4c1b5aed90232fc15f23620a77a79618f6bff8
Lacey Vu <laceylin2010@gmail.com>
2016-02-08 13:24:59 -0800

    rebuilt Twitter App. Cells are created and no errors. Working on appending Tweets to show

--- Twitter-Clone/Twitter-Clone/HomeViewController.swift
+++ Twitter-Clone/Twitter-Clone/HomeViewController.swift
@@ -1,6 +1,6 @@
 //
-//  ViewController.swift
-//  Twitter-Clone
+//  DetailViewController.swift
+//  Twitter Clone
 //
 //  Created by Lacey Vu on 2/8/16.
 //  Copyright © 2016 Lacey Vu. All rights reserved.
@@ -8,18 +8,74 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UITableViewDataSource
+{
+    @IBOutlet weak var tableView: UITableView!
+    var dataSource = [Tweet]() {
+        didSet {
+            //
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setupTableView()
+        
+        JSONParser.tweetJSONFrom(JSONParser.JSONData()) { (success, tweets) -> () in
+            
+        
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupTableView()
+    {
+        self.tableView.dataSource = self
+    }
+    
+    func update()
+    {
+        JSONParser.tweetJSONFrom(JSONParser.JSONData()) { (success, tweets) -> () in
+            if success  {
+                if let tweets = tweets {
+                    self.dataSource = tweets
+                }
+            }
+            
+        }
     }
 
+}
 
+extension HomeViewController
+{
+    func configureCellForIndexPath(indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let tweetCell = self.tableView.dequeueReusableCellWithIdentifier("tableCell", forIndexPath: indexPath)
+        let tweet = self.dataSource[indexPath.row]
+        tweetCell.textLabel?.text = tweet.text
+        return tweetCell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.dataSource.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        return self.configureCellForIndexPath(indexPath)
+    }
 }
 
